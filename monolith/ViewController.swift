7e7ee7d0f8705be25753188a1958f50ad6d97f10
Piece123 <taka.rock.i4@gmail.com>
2019-07-07 17:35:51 +0900

    Initial Commit

---
+++ monolith/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  monolith
+//
+//  Created by Takahiro Ishitsuka  on 2019/07/07.
+//  Copyright © 2019 Taka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
